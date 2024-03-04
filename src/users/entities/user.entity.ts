971dabef1651fb309e1e7db51e1d744945042550
fannihalmai <fanni.halmai@efrei.net>
2024-03-04 18:18:33 +0100

    feat(user): add user dto & service

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+export class User {
+  public id: number;
+  public username: string;
+  public firstname: string;
+  public lastname: string;
+  public email: string;
+  public password: string;
+
+  constructor(
+    id: number,
+    username: string,
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string,
+  ) {
+    this.id = id;
+    this.username = username;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.password = password;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  public readonly users = [
+    {
+      id: 1,
+      username: 'john',
+      firstname: 'John',
+      lastname: 'Doe',
+      password: 'changeme',
+      email: 'something',
+    },
+    {
+      id: 2,
+      firstname: 'John',
+      lastname: 'Doe',
+      username: 'maria',
+      password: 'guess',
+      email: 'something2',
+    },
+  ];
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOne(username: string): Promise<User | undefined> {
+    console.log('Looking for ', username, ' in ', this.users);
+    return this.users.find((user) => user.username === username);
   }
 }
